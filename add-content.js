1db16c6060a4e88f4f003255502ff6891ec0cbb3
Manuel Martinez <81333278+J-pixel73@users.noreply.github.com>
2024-02-11 18:33:41 -0600

    Add files via upload

---
+++ add-content.js
@@ -0,0 +1,65 @@
+var rccount = 0;
+var ycount = 0;
+var computer= document.getElementById('computer');
+var human= document.getElementById('human');
+var dialog= document.getElementById('dialog');
+function countClicks()
+//thats my loss
+{
+    var getComputerChoice = Math.floor(Math.random()*9);
+    if (rccount >=5){
+        rccount++ ;
+        var hsrc = document.getElementById('hsrc');
+        hsrc.src = 'meanKara.jpeg';
+    }
+    else if (getComputerChoice >= 6){
+        rccount++ ;
+        var hsrc = document.getElementById('hsrc');
+        hsrc.src = 'rock.jpeg';
+    } else if (getComputerChoice <= 2){
+        rccount++ ;
+        var hsrc = document.getElementById('hsrc');
+        hsrc.src = 'bam_Kara.jpeg';
+    } else if(getComputerChoice == 3){
+        rccount++ ;
+        var hsrc = document.getElementById('hsrc');
+        hsrc.src = 'crazy.jpeg';
+    }else if(getComputerChoice == 4){
+        rccount++ ;
+        var hsrc = document.getElementById('hsrc');
+        hsrc.src = 'osamabin.jpeg';}
+    if(rccount == 1){
+        dialog.innerHTML = 'Hey broo i think clicked the wrong button pleassssee click yes';
+    } else if(rccount == 2){
+        dialog.innerHTML = '';
+    } else if(rccount == 4){
+        dialog.innerHTML = 'YouR sTilL cLiCkinG nO!!';
+    } else if (rccount == 5){
+        dialog.innerHTML = 'No moRe ResPnses fOr you DaWg';}
+    else if(rccount == 6){
+            dialog.innerHTML = '';
+        }
+    }
+    console.log(rrcount);
+
+//thats my win
+function countClicks3()
+{
+    var hsrc = document.getElementById('hsrc');
+    hsrc.src = 'vaca_Loca.JPG';
+    var getComputerChoice = Math.floor(Math.random()*9);
+    if (rccount == 0 ){
+        dialog.innerHTML= "baby thats awsome!"
+        //this is my response to me getting chosen first
+    } else if (rccount == 1){
+        dialog.innerHTML= "your finger prolly slipped no worries baby"
+
+    } else if (rccount >= 1 && rccount <=4){
+        dialog.innerHTML= "osoma info leacked!"
+
+    } else if (rccount >= 5){
+        hsrc.src = 'meanKara.jpeg';
+        dialog.innerHTML= "nuh uhh taksys backsys"
+    } 
+    }
+
